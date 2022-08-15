d3ba61e60e824c0995786ca11800edd988c4dca2
kyj2212 <kyj2212@gmail.com>
2022-08-16 00:54:40 +0900

    Fix: remove create()

--- src/main/java/org/example/domain/member/service/MemberService.java
+++ src/main/java/org/example/domain/member/service/MemberService.java
@@ -13,14 +13,4 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Member create(String member_name, String id, String member_pwd, String member_email, Boolean member_login_status) {
-        Member member = new Member();
-        member.setMemberName(member_name);
-        member.setMemberId(id);
-        member.setMemberPwd(passwordEncoder.encode(member_pwd));
-        member.setMemberEmail(member_email);
-        member.setLoginStatus(member_login_status);
-        this.memberRepository.save(member);
-        return member;
-    }
 }
